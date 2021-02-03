72d677d0b31c7629e7abbf721209c3d876aa5be8
Sergey Temerkhanov <s.temerkhanov@gmail.com>
2021-02-03 13:18:26 +0000

    uefi: Support different build types (for debugging)
    Signed-off-by: Sergey Temerkhanov <s.temerkhanov@gmail.com>

--- pkg/uefi/build.sh
+++ pkg/uefi/build.sh
@@ -5,19 +5,21 @@
 
 set -e
 
+BUILD_TYPE=DEBUG
+
 case $(uname -m) in
-    aarch64) build -b RELEASE -t GCC5 -a AARCH64 -p ArmVirtPkg/ArmVirtQemu.dsc
-             cp Build/ArmVirtQemu-AARCH64/RELEASE_GCC5/FV/QEMU_EFI.fd OVMF.fd
-             cp Build/ArmVirtQemu-AARCH64/RELEASE_GCC5/FV/QEMU_VARS.fd OVMF_VARS.fd
+    aarch64) build -b ${BUILD_TYPE} -t GCC5 -a AARCH64 -p ArmVirtPkg/ArmVirtQemu.dsc
+             cp Build/ArmVirtQemu-AARCH64/${BUILD_TYPE}_GCC5/FV/QEMU_EFI.fd OVMF.fd
+             cp Build/ArmVirtQemu-AARCH64/${BUILD_TYPE}_GCC5/FV/QEMU_VARS.fd OVMF_VARS.fd
              # now let's build PVH UEFI kernel
              make -C BaseTools/Source/C
-             build -b RELEASE -t GCC5 -a AARCH64  -p ArmVirtPkg/ArmVirtXen.dsc
-             cp Build/ArmVirtXen-AARCH64/RELEASE_*/FV/XEN_EFI.fd OVMF_PVH.fd
+             build -b ${BUILD_TYPE} -t GCC5 -a AARCH64  -p ArmVirtPkg/ArmVirtXen.dsc
+             cp Build/ArmVirtXen-AARCH64/${BUILD_TYPE}_*/FV/XEN_EFI.fd OVMF_PVH.fd
              ;;
-     x86_64) build -b RELEASE -t GCC5 -a X64 -p OvmfPkg/OvmfPkgX64.dsc
-             cp Build/OvmfX64/RELEASE_*/FV/OVMF*.fd .
-             build -b RELEASE -t GCC5 -a X64 -p OvmfPkg/OvmfXen.dsc
-             cp Build/OvmfXen/RELEASE_*/FV/OVMF.fd OVMF_PVH.fd
+     x86_64) build -b ${BUILD_TYPE} -t GCC5 -a X64 -p OvmfPkg/OvmfPkgX64.dsc
+             cp Build/OvmfX64/${BUILD_TYPE}_*/FV/OVMF*.fd .
+             build -b ${BUILD_TYPE} -t GCC5 -a X64 -p OvmfPkg/OvmfXen.dsc
+             cp Build/OvmfXen/${BUILD_TYPE}_*/FV/OVMF.fd OVMF_PVH.fd
              ;;
           *) echo "Unsupported architecture $(uname). Bailing."
              exit 1
