3c25a0d0cac3249741f33f9b403b363a16f2c199
Sergey Temerkhanov <s.temerkhanov@gmail.com>
2021-02-04 12:57:26 +0000

    uefi: Enable serial port debug output
    Signed-off-by: Sergey Temerkhanov <s.temerkhanov@gmail.com>

--- pkg/uefi/build.sh
+++ pkg/uefi/build.sh
@@ -6,19 +6,20 @@
 set -e
 
 BUILD_TYPE=DEBUG
+DEFINES=-DDEBUG_ON_SERIAL_PORT=1
 
 case $(uname -m) in
-    aarch64) build -b ${BUILD_TYPE} -t GCC5 -a AARCH64 -p ArmVirtPkg/ArmVirtQemu.dsc
+    aarch64) build -b ${BUILD_TYPE} -t GCC5 -a AARCH64 -p ArmVirtPkg/ArmVirtQemu.dsc ${DEFINES}
              cp Build/ArmVirtQemu-AARCH64/${BUILD_TYPE}_GCC5/FV/QEMU_EFI.fd OVMF.fd
              cp Build/ArmVirtQemu-AARCH64/${BUILD_TYPE}_GCC5/FV/QEMU_VARS.fd OVMF_VARS.fd
              # now let's build PVH UEFI kernel
              make -C BaseTools/Source/C
-             build -b ${BUILD_TYPE} -t GCC5 -a AARCH64  -p ArmVirtPkg/ArmVirtXen.dsc
+             build -b ${BUILD_TYPE} -t GCC5 -a AARCH64  -p ArmVirtPkg/ArmVirtXen.dsc ${DEFINES}
              cp Build/ArmVirtXen-AARCH64/${BUILD_TYPE}_*/FV/XEN_EFI.fd OVMF_PVH.fd
              ;;
-     x86_64) build -b ${BUILD_TYPE} -t GCC5 -a X64 -p OvmfPkg/OvmfPkgX64.dsc
+     x86_64) build -b ${BUILD_TYPE} -t GCC5 -a X64 -p OvmfPkg/OvmfPkgX64.dsc ${DEFINES}
              cp Build/OvmfX64/${BUILD_TYPE}_*/FV/OVMF*.fd .
-             build -b ${BUILD_TYPE} -t GCC5 -a X64 -p OvmfPkg/OvmfXen.dsc
+             build -b ${BUILD_TYPE} -t GCC5 -a X64 -p OvmfPkg/OvmfXen.dsc ${DEFINES}
              cp Build/OvmfXen/${BUILD_TYPE}_*/FV/OVMF.fd OVMF_PVH.fd
              ;;
           *) echo "Unsupported architecture $(uname). Bailing."
