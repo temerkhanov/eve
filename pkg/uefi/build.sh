e230811aa2d1ef529649e83756e0b5c4b454d12d
Sergey Temerkhanov <s.temerkhanov@gmail.com>
2021-02-05 02:34:37 +0000

    uefi: Update build script
    Signed-off-by: Sergey Temerkhanov <s.temerkhanov@gmail.com>

--- pkg/uefi/build.sh
+++ pkg/uefi/build.sh
@@ -6,20 +6,22 @@
 set -e
 
 BUILD_TYPE=DEBUG
-DEFINES=-DDEBUG_ON_SERIAL_PORT=1
+DEFINES=-DDEBUG_ON_SERIAL_PORT
+NCPUS=$(getconf _NPROCESSORS_ONLN)
+
+make -j ${NCPUS} -C BaseTools/Source/C
 
 case $(uname -m) in
-    aarch64) build -b ${BUILD_TYPE} -t GCC5 -a AARCH64 -p ArmVirtPkg/ArmVirtQemu.dsc ${DEFINES}
+    aarch64) build -b ${BUILD_TYPE} -t GCC5 -a AARCH64 -p ArmVirtPkg/ArmVirtQemu.dsc -n ${NCPUS} ${DEFINES}
              cp Build/ArmVirtQemu-AARCH64/${BUILD_TYPE}_GCC5/FV/QEMU_EFI.fd OVMF.fd
              cp Build/ArmVirtQemu-AARCH64/${BUILD_TYPE}_GCC5/FV/QEMU_VARS.fd OVMF_VARS.fd
              # now let's build PVH UEFI kernel
-             make -C BaseTools/Source/C
-             build -b ${BUILD_TYPE} -t GCC5 -a AARCH64  -p ArmVirtPkg/ArmVirtXen.dsc ${DEFINES}
+             build -b RELEASE -t GCC5 -a AARCH64  -p ArmVirtPkg/ArmVirtXen.dsc -n ${NCPUS} ${DEFINES}
              cp Build/ArmVirtXen-AARCH64/${BUILD_TYPE}_*/FV/XEN_EFI.fd OVMF_PVH.fd
              ;;
-     x86_64) build -b ${BUILD_TYPE} -t GCC5 -a X64 -p OvmfPkg/OvmfPkgX64.dsc ${DEFINES}
+     x86_64) build -b ${BUILD_TYPE} -t GCC5 -a X64 -p OvmfPkg/OvmfPkgX64.dsc -n ${NCPUS} ${DEFINES}
              cp Build/OvmfX64/${BUILD_TYPE}_*/FV/OVMF*.fd .
-             build -b ${BUILD_TYPE} -t GCC5 -a X64 -p OvmfPkg/OvmfXen.dsc ${DEFINES}
+             build -b ${BUILD_TYPE} -t GCC5 -a X64 -p OvmfPkg/OvmfXen.dsc -n ${NCPUS} ${DEFINES}
              cp Build/OvmfXen/${BUILD_TYPE}_*/FV/OVMF.fd OVMF_PVH.fd
              ;;
           *) echo "Unsupported architecture $(uname). Bailing."
